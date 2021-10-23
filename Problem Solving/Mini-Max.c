d64c10d99ac0a7bad2af4264285a36854c1ee3e1
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2021-10-23 15:31:38 +0530

    Update Mini-Max.c

--- Problem Solving/Mini-Max.c
+++ Problem Solving/Mini-Max.c
@@ -1,3 +1,4 @@
+// Method 1:-
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
@@ -26,3 +27,51 @@ int main() {
     printf("%ld %ld",sum-max,sum-min);
     return 0;
 }
+//Method :2
+
+
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    int i,j,temp;
+    long long int sum1=0,sum2=0;
+    int arr[5];
+    
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<5;j++)
+        if(arr[j]>arr[i] )
+           {
+            temp=arr[j];
+            arr[j]=arr[i];
+            arr[i]=temp;
+           }
+    }
+       
+    for(i=0;i<4;i++)
+    {
+        sum1 += arr[i];
+    }
+    for(i=1;i<5;i++)
+    {
+        sum2 += arr[i];
+    }
+    
+    printf("%lli %lli",sum1,sum2);
+    
+}
+
