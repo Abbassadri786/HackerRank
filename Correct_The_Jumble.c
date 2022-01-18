253720144863002f24a31d5794e752b6b086e8f3
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2022-01-18 20:02:29 +0530

    Update Correct_The_Jumble.c

--- Correct_The_Jumble.c
+++ Correct_The_Jumble.c
@@ -1,63 +1,3 @@
-//METHOD 1
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int main()
-{
-   long long n1,n2,t,i=0,j=0,c1=0,c2=0;
-   cin>>t;
-   
-   int arr1[100],arr2[100];
-   
-   for(i=0 ; i<t; i++)
-   {
-       cin>>n1>>n2;
-       
-       while(n1>0)
-       {
-           
-           arr1[i] = n1%2;
-           n1 = n1/2;
-           i++;
-           c1++;
-       }
-       
-       while(n2>0)
-       {
-           arr2[j] = n2%2;
-           n2 = n2/2;
-           j++;
-           c2++;
-           
-       }
-   }
-   long long count=0;
-   
-   if(c2>c1)
-   {
-       for(i=0,j=0; j<c2 ; i++,j++)
-       {
-           if(arr1[i]!=arr2[j])
-           count++;
-       }
-       cout<<count<<endl;
-       
-   }
-   else{
-       for(i=0,j=0; i<c1 ; i++,j++)
-       {
-           if(arr1[i]!=arr2[j])
-           count++;
-       }
-       cout<<count<<endl;
-   }
-   
-   return 0;
-}
-
-//METHOD 2
-
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -81,14 +21,15 @@ int main()
            j++;
            c1++;
        }
-   }
-   long long count=0;
-   for(j=0 ; j<c1; j++)
-   {
+       long long count=0;
+       for(j=0 ; j<c1; j++)
+       {
        if(arr1[j]==1)
        count++;
+       }
+       cout<<count<<endl;
    }
-   cout<<count<<endl;
+   
    
    return 0;
 }
